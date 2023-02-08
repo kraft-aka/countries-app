97bc480e7ede0bb982b7f6b7d070c74bf9b5f6c5
kubat <kubat.aiupov@gmail.com>
2023-02-08 21:41:53 +0100

    init getAllData func, displays data

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 const url = 'https://restcountries.com/v3.1/all'
+const output = document.querySelector('.output')
 
 const fetchData = async (endpoint) => {
-  const response = await fetch(endpoint);
-  if(response.ok) {
-    const responseJson = await response.json();
-    console.log(responseJson)
-    getRawData(responseJson)
+  try {
+
+    const response = await fetch(endpoint);
+    if(response.ok) {
+      const responseJson = await response.json();
+      console.log(responseJson)
+      // getRawData(responseJson)
+      getAllData(responseJson)
+    }
+  } catch(err) {
+    console.log(err)
   }
 };
 
 const getRawData = (data) => {
-  const output = document.querySelector('.output')
   output.textContent = JSON.stringify(data, undefined, 2)
 }
 
+const getAllData = (data) => {
+  const list =[];
+  let country;
+  data.forEach(item => {
+   country = `<div id="country-item">
+   <p>name: ${item.name.common}</p>
+   <p>area: ${item.area}km2</p>
+   <p>capital city: ${item.capital || ''}</p>
+   <p>timezone: ${item.timezones}</p>
+   <p>population: ${item.population}</p>
+   <img src='${item.flags.svg}' id="country-img" width="100px"/> 
+   </div>`
+   list.push(country)
+  })
+  output.innerHTML = list.join('');
 
+}
 
 fetchData(url)
